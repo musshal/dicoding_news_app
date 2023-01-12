c9e270f49cf42b91bf65f8289265f52ac533a23a
Musthafa Faishal <musthafafaishal@gmail.com>
2023-01-12 17:27:25 +0700

    first commit

---
+++ lib/detail_page.dart
@@ -0,0 +1,55 @@
+import 'package:dicoding_news_app/article.dart';
+import 'package:dicoding_news_app/article_web_view.dart';
+import 'package:flutter/material.dart';
+
+class ArticleDetailPage extends StatelessWidget {
+  static const routeName = '/article_detail';
+
+  final Article article;
+
+  const ArticleDetailPage({Key? key, required this.article}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(article.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network(article.urlToImage),
+            Padding(padding: const EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  article.title,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                  ),
+                ),
+                const Divider(color: Colors.grey),
+                Text('Date: ${article.publishedAt}'),
+                const SizedBox(height: 10),
+                Text('Author: ${article.author}'),
+                const Divider(color: Colors.grey),
+                Text(
+                  article.content,
+                  style: const TextStyle(fontSize: 10),
+                ),
+                const SizedBox(height: 10),
+                ElevatedButton(onPressed: () {
+                  Navigator.pushNamed(context, ArticleWebView.routeName,
+                      arguments: article.url);
+                }, child: const Text('Read more'))
+              ],
+            ),)
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:dicoding_news_app/article.dart';
+import 'package:dicoding_news_app/article_web_view.dart';
+import 'package:dicoding_news_app/detail_page.dart';
+import 'package:flutter/material.dart';
+import 'package:dicoding_news_app/news_list_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'News App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: NewsListPage.routeName,
+      routes: {
+        NewsListPage.routeName: (context) => const NewsListPage(),
+        ArticleDetailPage.routeName: (context) => ArticleDetailPage(article: ModalRoute.of(context)?.settings.arguments as Article),
+        ArticleWebView.routeName: (context) => ArticleWebView(url: ModalRoute.of(context)?.settings.arguments as String)
+      }
+    );
+  }
+}
