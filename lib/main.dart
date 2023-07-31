49b6e8f7cecbb81feb23dd796f1ae17eff6fdae8
Musthafa Faishal <musthafafaishal@gmail.com>
2023-07-31 22:24:09 +0700

    style: add custom widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:dicoding_news_app/article.dart';
 import 'package:dicoding_news_app/article_web_view.dart';
 import 'package:dicoding_news_app/detail_page.dart';
+import 'package:dicoding_news_app/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:dicoding_news_app/news_list_page.dart';
 
@@ -16,8 +17,25 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'News App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        colorScheme: Theme.of(context).colorScheme.copyWith(
+          primary: primaryColor,
+          onPrimary: Colors.black,
+          secondary: secondaryColor
+        ),
+        textTheme: myTextTheme,
+        appBarTheme: const AppBarTheme(elevation: 0),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: secondaryColor,
+            foregroundColor: Colors.white,
+            textStyle: const TextStyle(),
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(0)
+              )
+            )
+          )
+        )
       ),
       initialRoute: NewsListPage.routeName,
       routes: {
